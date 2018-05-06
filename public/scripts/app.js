463b248ddcf44dfa7f0854e4b4cfe051fe7f27ca
Chris Ewald <itsbewald@gmail.com>
2018-05-05 17:17:08 -0700

    Completed basic functionality of app. User can type something and input it. The list will re-render without page load to show added item on list. User can click 'What should I do?' to have the app select a random item from the list for them. User can also click 'Remove All' to empty the list of items and re-render a blank list.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,56 +1,37 @@
 "use strict";
 
-console.log("App.js is running!");
-
-var app = {
-	title: "Indecision App",
-	subtitle: "Let me make the decision for you",
-	options: ['one', 'two']
+var displayProps = {
+	displayBool: false
 };
-// JSX - JavaScript XML
-function getSubtitle(sub) {
-	if (sub) {
-		return React.createElement(
-			"p",
-			null,
-			sub
-		);
-	}
-}
 
-var template = React.createElement(
-	"div",
-	null,
-	React.createElement(
-		"h1",
-		null,
-		app.title
-	),
-	app.subtitle && React.createElement(
-		"p",
-		null,
-		app.subtitle
-	),
-	React.createElement(
-		"p",
-		null,
-		app.options.length ? "Here are your options: " : "There are currently no options"
-	),
-	React.createElement(
-		"ol",
-		null,
-		React.createElement(
-			"li",
-			null,
-			"first item"
-		),
-		React.createElement(
-			"li",
-			null,
-			"second item"
-		)
-	)
-);
+var toggleVisibility = function toggleVisibility() {
+	displayProps.displayBool = !displayProps.displayBool;
+	render();
+};
 
 var appRoot = document.getElementById("app");
-ReactDOM.render(template, appRoot);
+
+var render = function render() {
+	var template = React.createElement(
+		"div",
+		null,
+		React.createElement(
+			"h1",
+			null,
+			"Visibility Toggle"
+		),
+		React.createElement(
+			"button",
+			{ onClick: toggleVisibility },
+			displayProps.displayBool ? "Hide Details" : "Show Details"
+		),
+		displayProps.displayBool ? React.createElement(
+			"p",
+			null,
+			"Hey. These are some details you can now see!"
+		) : false
+	);
+	ReactDOM.render(template, appRoot);
+};
+
+toggleVisibility();
--- src/app.js
+++ src/app.js
@@ -3,29 +3,65 @@ console.log("App.js is running!");
 const app = {
 	title: "Indecision App",
 	subtitle: "Let me make the decision for you",
-	options: ['one', 'two']
+	options: []
 };
 // JSX - JavaScript XML
 function getSubtitle(sub) {
 	if (sub) {
 		return <p>{sub}</p>;
 	}
+};
+
+const onFormSubmit = (e) => {
+	e.preventDefault();
+
+	const option = e.target.elements.option.value;
+
+	if (option) {
+		app.options.push(option);
+		e.target.elements.option.value = "";
+		render();	
+	}	
+};
+
+const onRemoveAll = () => {
+	app.options = [];
+	render();
+}
+
+const onMakeDecision = () => {
+	const randIndex = Math.floor(Math.random() * app.options.length);
+	const decisionMade = app.options[randIndex];
+	alert(decisionMade);
+	app.options = [];
+	render();
 }
 
 
-const template = (
-	<div>
-		<h1>{app.title}</h1>
-		{app.subtitle && <p>{app.subtitle}</p>}
-		<p>{app.options.length ? "Here are your options: " : "There are currently no options"}</p>
-		
-		<ol>
-			<li>first item</li>
-			<li>second item</li>
-		</ol>
-		
-	</div>
-);
-
 const appRoot = document.getElementById("app");
-ReactDOM.render(template, appRoot);
+
+const render = () => {
+	const template = (
+		<div>
+			<h1>{app.title}</h1>
+			{app.subtitle && <p>{app.subtitle}</p>}
+			<p>{app.options.length ? "Here are your options: " : "There are currently no options"}</p>
+			<button disabled={!app.options.length} onClick={onMakeDecision}>What should I do?</button>
+			<button onClick={onRemoveAll}>Remove All</button>
+			
+			<ol>
+				{
+					app.options.map((option) => <li key={option}>{option}</li>)
+				}
+			</ol>
+			<form onSubmit={onFormSubmit}>
+				<input type="text" name="option"/>
+				<button>Add Option</button>
+			</form>
+		</div>
+	);
+	ReactDOM.render(template, appRoot);
+}
+
+render();
+
