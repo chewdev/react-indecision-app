092f8706b4edb46fa2ab3b45e18afb2c44600588
Chris Ewald <itsbewald@gmail.com>
2018-05-06 03:05:31 -0700

    Refactored both build-it-visibile and counter-example to use Components and states to update and render changes from UI

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,25 +1,32 @@
-const displayProps = {
-	displayBool: false
+class SecretMessage extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleVisibilityToggle = this.handleVisibilityToggle.bind(this);
+		this.state = {
+			visibility: false,
+			buttonText: 'Show Details'
+		}
+	}
+
+	handleVisibilityToggle() {
+		this.setState((prevState) => {
+			return {
+				visibility: !prevState.visibility,
+				buttonText: prevState.visibility ? 'Show Details' : 'Hide Details'
+			};
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Visibility Toggle</h1>
+				<button onClick={this.handleVisibilityToggle}>{this.state.buttonText}</button>
+				{this.state.visibility ? <p>Hey. These are some details!</p> : false}
+			</div>
+			)
+	}
 }
 
-const toggleVisibility = () => {
-	displayProps.displayBool = !displayProps.displayBool;
-	render();
-};
+ReactDOM.render(<SecretMessage />, document.getElementById("app"));
 
-const appRoot = document.getElementById("app");
-
-const render = () => {
-	const template = (
-		<div>
-			<h1>Visibility Toggle</h1>
-			<button onClick={toggleVisibility}>{displayProps.displayBool ? "Hide Details" : "Show Details"}</button>
-			{displayProps.displayBool ? <p>Hey. These are some details you can now see!</p> : false}
-		</div>
-	);
-	ReactDOM.render(template, appRoot);
-};
-
-
-
-toggleVisibility();
